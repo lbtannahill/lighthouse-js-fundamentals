ffd9e63b160363d10357f6dfbb1353be848a7171
Lindsay <lbtannahill@gmail.com>
2021-12-29 15:01:39 -0800

    creating an isEven function

---
+++ even.js
@@ -0,0 +1,10 @@
+const isEven = function (num) {
+ return num % 2 === 0;
+}
+
+const tentIsEven = isEven(10);
+const elevenIsEven = isEven(11);
+
+console.log(isEven(10));
+console.log(isEven(11));
+
